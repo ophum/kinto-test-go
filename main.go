f26aea6580f25cfbc53c17929b4f3d1749e3af23
ophum <inagaki0106@gmail.com>
2020-03-13 19:58:14 +0900

    1st commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.String(200, "Hello kinto!")
+	})
+
+	r.Run()
+}
